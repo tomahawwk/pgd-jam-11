fa58cba4d6e95d2cc4a8c6bc811b2ca3ca6c6b69
Aliery-sx <68945354+Aliery-sx@users.noreply.github.com>
2023-02-05 17:00:12 +0300

    Buyn++

--- Assets/Prototype (Aliery)/NPC/InteractiveBYUN.cs
+++ Assets/Prototype (Aliery)/NPC/InteractiveBYUN.cs
@@ -1,54 +1,113 @@
 using Dialogue;
+using Prototype.Logic;
 using Prototype.Plugins.FadeOutSystem;
+using SaveState;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Prototype
 {
+    [RequireComponent(typeof(AudioSource))]
     public class InteractiveBYUN : Interactive
     {
-        private DialogueSystem _dialogue => DialogueSystem.Instance;
-        private FadeOutSystem _fadeOut => FadeOutSystem.Instance;
+        private const string BunTitle = "Баюн";
+        private const string YagaTitle = "Яга";
 
-        [SerializeField] private Sprite _catByun;
-        [SerializeField] private Sprite _babaYaga;
+        private string SaveFirstDialogue = nameof(InteractiveBYUN) + "Quest";
+        private string SaveHasItem = nameof(InteractiveBYUN) + "Has Quest Item";
 
-        private bool _firstDialogue=true;
+        [SerializeField] private GameObject _openBake;
+        [SerializeField] private GameObject _closeBake;
+        [SerializeField] private GameObject _bakeItem;
+
+
+
+        private AudioSource _audio => GetComponent<AudioSource>();
+        private DialogueSystem _dialogueSystem => DialogueSystem.Instance;
+        private SaveStateSystem _saveStateSystem => SaveStateSystem.Instance;
+        private InventorySystem _inventorySystem => InventorySystem.Instance;
+        private FadeOutSystem _fade => FadeOutSystem.Instance;
+
+        [SerializeField] private Sprite _bunAvatar;
+        [SerializeField] private Sprite _agaAvatar;
+
+        [SerializeField] private List<Item> _items;
+
+        public Item GetItem()
+        {
+            foreach (var item in _items)
+            {
+                if (InventorySystem.Instance.HasItem(item))
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        private void SayBun(string text) => _dialogueSystem.DialogueAvatar(BunTitle, text, _bunAvatar);
+        private void AgaSay(string text) => _dialogueSystem.DialogueAvatar(YagaTitle, text, _agaAvatar);
+
+        private void SayBun(string text, string question1, string question2, Action<bool> result)
+        {
+            _dialogueSystem.DialogueDoubleQuestion(_bunAvatar, BunTitle, text, question1, question2, result);
+        }
+
+        private void SayAga(string text, string question1, string question2, Action<bool> result)
+        {
+            _dialogueSystem.DialogueDoubleQuestion(_agaAvatar, YagaTitle, text, question1, question2, result);
+        }
+
+       
+
+        private void Start()
+        {
+            CheckStateBun();
+        }
+        private void CheckStateBun()
+        {
+            if (_saveStateSystem.GetState(SaveFirstDialogue))
+            {
+                _openBake.SetActive(true);
+                _closeBake.SetActive(false);
+            }
+
+            //if (_saveStateSystem.GetState(SaveHasItem))
+            //{
+            //    _bakeItem.SetActive(false);
+            //}
+        }
 
         public override void Interact()
         {
-            if (_firstDialogue)
+            if (!_saveStateSystem.GetState(SaveFirstDialogue))
             {
-                _firstDialogue = false;
-                _dialogue.DialogueAvatar("Кот Баюн", "С добрым утречком, хозяйка зверей, покровительница людей! Как спалось? Кости на погоду не ломило?", _catByun);
-                _dialogue.DialogueAvatar("Баба Яга", "Уж не ты ли, пройдоха, свою лапу тут приложил, да избушку околдовал?", _babaYaga);
-                _dialogue.DialogueAvatar("Кот Баюн", "Не я, хозяйка, не я. Но может и следовало — давно ты меня <color=yellow>рыбкой</color> не потчевала, на печи давно полежать не пускала…", _catByun);
-                _dialogue.DialogueAvatar("Баба Яга", "А чьих это рук дело, стало быть?", _babaYaga);
-                _dialogue.DialogueAvatar("Кот Баюн", "А они все рядом тут — ты оглядись по всем четырем сторонам, да под ноги поглядывай. Думается мне, из избушки не только ты выпала, но и всяческое барахло.", _catByun);
-                _dialogue.DialogueAvatar("Кот Баюн", "Авось, пригодится чего-нибудь и соседям твоим, тогда они сведут свое колдовство.", _catByun);
+                SayBun("С добрым утречком, хозяйка зверей, покровительница людей! Как спалось? Кости на погоду не ломило?");
+                AgaSay("Уж не ты ли, пройдоха, свою лапу тут приложил, да избушку околдовал?");
+                SayBun("Не я, хозяйка, не я. Но может и следовало — давно ты меня <color=yellow>рыбкой</color> не потчевала, на печи давно полежать не пускала…");
+                AgaSay("А чьих это рук дело, стало быть?");
+                SayBun("А они все рядом тут — ты оглядись по всем четырем сторонам, да под ноги поглядывай. Думается мне, из избушки не только ты выпала, но и всяческое барахло.");
+                SayBun("Авось, пригодится чего-нибудь и соседям твоим, тогда они сведут свое колдовство.");
+                _saveStateSystem.SaveState(SaveFirstDialogue, true);
             }
             else
             {
-                _dialogue.DialogueDoubleQuestion(_catByun, "Кот Баюн", "Чем помочь, хозяйка?",
-                "Лестница", "Ничем, пока", result =>
-                {
-                    if (result)
+
+                SayBun("Чем помочь, хозяйка?",
+                    "Лестница", "Ничем, пока", result =>
                     {
-                        _dialogue.DialogueDoubleQuestion(_catByun, "Кот Баюн", "Вот и дожила ты, старая, до того, что сама по деревьям лазаешь…",
-                        "О, спасибо", "Мог бы и помочь!", result =>
-                        {
                         if (result)
-                            _dialogue.DialogueAvatar("Кот Баюн", "Незачто", _catByun);
+                        {
+                            SayBun("Типа помог");
+                        }
                         else
-                            _dialogue.DialogueAvatar("Кот Баюн", "Сама справишься!", _catByun);
-                        });
-                    }
-                    else
-                    {
-                        _dialogue.DialogueAvatar("Кот Баюн", "Бывай!", _catByun);
-                    }
-                }
-            );
+                        {
+                            SayBun("Бывай!");
+                        }
+                    });
             }
         }
     }
